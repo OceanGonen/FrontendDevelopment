6a6d0f778ba660f6fc5377b500a5d4d200281355
OceanGonen <ocean.gonen@hotmail.com>
2025-01-17 11:36:28 +0100

    hopefullyfinal

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,70 @@ document.querySelector('header > button').addEventListener('click', () => {
 
   }
 });
+
+
+// Table collpase
+
+const prevDayButton = document.querySelector("main > section > button:nth-of-type(1)");
+const nextDayButton = document.querySelector("main > section > button:nth-of-type(2)");
+const table = document.querySelector("table");
+
+const minNumber = 2;
+const maxNumber = 8;
+
+// GO TO NEXT DAY
+nextDayButton.onclick = goToNextDay;
+
+function goToNextDay() {	
+	// determine new number
+	let huidigeNummer = parseInt( table.dataset.number );
+	let nieuweNummer = huidigeNummer + 1;
+
+	
+  // check buttons 	
+	prevDayButton.disabled = false;
+	if (nieuweNummer == maxNumber) {
+		nextDayButton.disabled = true;
+	}
+	
+	// check and set new number
+	if (nieuweNummer > maxNumber) {
+		nieuweNummer = maxNumber;
+	}
+	table.dataset.number = nieuweNummer;
+}
+
+// GO TO PREVIOUS DAY
+prevDayButton.onclick = goToPrevDay;
+
+function goToPrevDay() {	
+	// determine new number
+	let huidigeNummer = parseInt( table.dataset.number );
+	let nieuweNummer = huidigeNummer - 1;
+	
+	// check buttons 	
+	nextDayButton.disabled = false;
+	if (nieuweNummer == minNumber) {
+		prevDayButton.disabled = true;
+	}
+	
+	// check and set new number
+	if (nieuweNummer < minNumber) {
+		nieuweNummer = minNumber;
+	}
+	table.dataset.number = nieuweNummer;
+}
+
+
+//BACK TO TOP 
+
+const backToTopButton = document.querySelector("main>a:nth-of-type(2)");
+
+backToTopButton.onclick = scrollToTop; 
+
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
